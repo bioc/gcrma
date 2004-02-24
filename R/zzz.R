3219bc7238eb6d8b4ff67a62fa2f29ac9a65a284
dfcimm1 <unknown>
2004-02-24 17:58:40 +0000

    RI: added a zzz.R file to load affy and upped the DESCRIPTION
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/gcrma@7502 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/zzz.R
@@ -0,0 +1,10 @@
+.First.lib <- function(libname, pkgname, where) {
+  
+
+  where <- match(paste("package:", pkgname, sep=""), search())
+
+  require(affy, quietly=TRUE) ##Biobase uses methods
+
+  cacheMetaData(as.environment(where))
+
+}

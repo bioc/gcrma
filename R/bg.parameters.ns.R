c9a2acc59a84b9dcc2b68b45c940d3068ea947f8
Wu Zhijin (Jean) <zwu@jhsph.edu>
2007-02-26 03:52:27 +0000

    zwu:Feb2507 fixbug in affinity range
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/gcrma@22827 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/bg.parameters.ns.R
+++ R/bg.parameters.ns.R
@@ -1,15 +1,18 @@
 bg.parameters.ns=function (x, affinities,affinities2=NULL,affinities3=NULL)
 {   
-       set.seed(1989);sample1=sample(length(x),5000)
-       suppressWarnings(lo1<-loess(log(x)~affinities,
+       set.seed(1989)
+       O=order(affinities)
+       sample1=sample(length(x),5000)
+       a1 <-affinities[O] 
+       suppressWarnings(lo1<-loess(log(x)[O]~a1,
                         subset=sample1,degree=1,family="symmetric",span=.2))
        bg.mu=predict(lo1,affinities)
-       bg.mu[affinities>max(affinities[sample1])]=max(lo1$fitted)
-       bg.mu[affinities<min(affinities[sample1])]=min(lo1$fitted)
+       bg.mu[affinities>max(a1[sample1])]=max(lo1$fitted)
+       bg.mu[affinities<min(a1[sample1])]=min(lo1$fitted)
        if (is.null(affinities2)) bg.mu2=NULL
        else {bg.mu2=predict(lo1,affinities2)
-             bg.mu2[affinities2>max(affinities[sample1])]=max(lo1$fitted)
-             bg.mu2[affinities2<min(affinities[sample1])]=min(lo1$fitted)
+             bg.mu2[affinities2>max(a1[sample1])]=max(lo1$fitted)
+             bg.mu2[affinities2<min(a1[sample1])]=min(lo1$fitted)
            }
        res=lo1$res[lo1$res<0];res=c(res,-res)
        bg.sigma=mad(res)
@@ -17,8 +20,8 @@ bg.parameters.ns=function (x, affinities,affinities2=NULL,affinities3=NULL)
        if (is.null(affinities3))        return(list(bg.mu=bg.mu,bg.mu2=bg.mu2,bg.sigma=bg.sigma))
        
        else {bg.mu3=predict(lo1,affinities3)
-             bg.mu3[affinities3>max(affinities[sample1])]=max(lo1$fitted)
-             bg.mu3[affinities3<min(affinities[sample1])]=min(lo1$fitted)
+             bg.mu3[affinities3>max(a1[sample1])]=max(lo1$fitted)
+             bg.mu3[affinities3<min(a1[sample1])]=min(lo1$fitted)
              return(list(bg.mu=bg.mu,bg.mu2=bg.mu2,bg.mu3=bg.mu3,bg.sigma=bg.sigma))
            }
      }
